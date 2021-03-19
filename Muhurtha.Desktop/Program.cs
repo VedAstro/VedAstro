73d6e9d583add905144f630941690512428e9591
Sengiv <vignes@live.com.my>
2021-03-20 05:10:16 +0800

    MK1.0

---
+++ Muhurtha.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Muhurtha.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

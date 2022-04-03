a5033c782c6713233ca10a82e8be7b285f6fb0ed
Vignes <vignes@live.com.my>
2022-04-03 08:07:41 +0800

    5.15 - dasa view make current year pop

--- Website/Managers/GlobalVariableManager.cs
+++ Website/Managers/GlobalVariableManager.cs
@@ -8,10 +8,27 @@ namespace Website
     /// </summary>
     public class GlobalVariableManager
     {
+
+        /// <summary>
+        /// Init what ever global data that can be initialized
+        /// </summary>
+        public GlobalVariableManager()
+        {
+            
+        }
+
+
+
         /// <summary>
         /// This instance is referencing the AlertMessage located in MainLayout
         /// </summary>
         public AlertMessage Alert { get; set; }
+
+        /// <summary>
+        /// The current time in the system
+        /// </summary>
+        public DateTimeOffset SystemTimeNow => DateTimeOffset.Now;
+
 
     }
 }

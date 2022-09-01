9410535aff5035ddad6877f884357c0ad7af75f7
Sengiv <43817262+sengiv@users.noreply.github.com>
2022-09-01 17:20:25 +0800

    8.52 - date picker bug fix

--- Website/wwwroot/js/TimeInput.js
+++ Website/wwwroot/js/TimeInput.js
@@ -72,6 +72,7 @@ export function LoadCalendar(hour12, minute, meridian, date, month, year) {
             //update year & month immediately even though not yet click date
             //allows user to change only month or year
             clickMonth(e, month) {
+                month = month + 1; //correction for jb lib bug
                 var with0 = ('0' + month).slice(-2);//change 9 to 09
                 monthInputElm.value = with0;
             },

cd137fc694a90adbf6c3bf131d7145c17cabd4b1
VedAstroDeveloper <developer@vedastro.org>
2022-10-12 15:27:05 +0800

    9.41 - updates

---
+++ APITests/EntryPointTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Tests
+{
+    [TestClass()]
+    public class EntryPointTests
+    {
+        [TestMethod()]
+        public void GetSummaryColorTest()
+        {
+            var x = EntryPoint.GetSummaryColor(1, -5, 6);
+            Console.WriteLine(x);
+            var y = EntryPoint.GetSummaryColor(4, -5, 6);
+            Console.WriteLine(y);
+            Assert.Fail();
+        }
+    }
+}

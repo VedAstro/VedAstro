7fb9a2359d5950b1140e750c73b9182eed2f5e7f
VedAstroDeveloper <developer@vedastro.org>
2023-01-17 12:51:02 +0800

    10.95 - events chart minor fix

--- Website/wwwroot/js/EventsChart.js
+++ Website/wwwroot/js/EventsChart.js
@@ -589,8 +589,8 @@ class EventsChart {
         //FUNCTIONS
         function drawDescriptionBox(eventDesc) {
             //if no description than hide box & end here
-            //if (!eventDesc) { $("#CursorLineLegendDescriptionHolder").hide(); return; }
-            if (!eventDesc) { this.$CursorLineLegendDescriptionHolder.hide(); return; }
+            if (!eventDesc) { $("#CursorLineLegendDescriptionHolder").hide(); return; }
+            //if (!eventDesc) { this.$CursorLineLegendDescriptionHolder.hide(); return; }
 
             //convert text to svg and place inside holder
             var wrappedDescText = EventsChart.textToSvg(eventDesc, 175, 24);

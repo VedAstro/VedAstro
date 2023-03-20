92561401c0728bdc925706fa896fbb4b5342b1d7
VedAstroDeveloper <developer@vedastro.org>
2023-03-20 18:32:15 +0800

    open API includes 7 types of planet strengths

--- API/OpenAPI.cs
+++ API/OpenAPI.cs
@@ -10,8 +10,8 @@ namespace API
         /// <summary>
         /// https://www.vedastro.org/api/Location/Singapore/Time/23:59/31/12/2000/+08:00/Planet/Sun/Sign/
         /// </summary>
-        [Function("Location")]
-        public static async Task<HttpResponseData> Main([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Location/{locationName}/Time/{hhmmStr}/{dateStr}/{monthStr}/{yearStr}/{offsetStr}/Planet/{planetNameStr}/{propertyName}")] HttpRequestData incomingRequest, string locationName, string hhmmStr, string dateStr, string monthStr, string yearStr, string offsetStr, string planetNameStr, string propertyName)
+        [Function(nameof(OneProperty))]
+        public static async Task<HttpResponseData> OneProperty([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Location/{locationName}/Time/{hhmmStr}/{dateStr}/{monthStr}/{yearStr}/{offsetStr}/Planet/{planetNameStr}/{propertyName}")] HttpRequestData incomingRequest, string locationName, string hhmmStr, string dateStr, string monthStr, string yearStr, string offsetStr, string planetNameStr, string propertyName)
         {
             //log the call
             APILogger.Visitor(incomingRequest);
@@ -41,10 +41,79 @@ namespace API
                 case "Declination": returnVal = AstronomicalCalculator.GetPlanetDeclination(planetName, parsedTime).ToString(); break;
                 case "AspectingPlanets": returnVal = AstronomicalCalculator.GetPlanetsAspectingPlanet(parsedTime, planetName).ToString(); break;
                 case "Motion": returnVal = AstronomicalCalculator.GetPlanetMotionName(planetName, parsedTime).ToString(); break;
-                case "FixedLongitude":
-                case "NirayanaLongitude": returnVal = AstronomicalCalculator.GetPlanetNirayanaLongitude(parsedTime, planetName).ToString(); break;
-                case "MovableLongitude":
-                case "SayanaLongitude": returnVal = AstronomicalCalculator.GetPlanetSayanaLongitude(parsedTime, planetName).ToString(); break;
+            }
+
+
+            return APITools.PassMessage(returnVal, incomingRequest);
+        }
+
+        /// <summary>
+        /// Here comes calls that with 2 level properties like planet strength
+        /// https://www.vedastro.org/api/Location/Singapore/Time/23:59/31/12/2000/+08:00/Planet/Sun/Strength/Temporal
+        /// </summary>
+        [Function(nameof(TwoProperty))]
+        public static async Task<HttpResponseData> TwoProperty([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Location/{locationName}/Time/{hhmmStr}/{dateStr}/{monthStr}/{yearStr}/{offsetStr}/Planet/{planetNameStr}/{propertyName1}/{propertyName2}")] HttpRequestData incomingRequest, string locationName, string hhmmStr, string dateStr, string monthStr, string yearStr, string offsetStr, string planetNameStr, string propertyName1, string propertyName2)
+        {
+            //log the call
+            APILogger.Visitor(incomingRequest);
+
+            PlanetName planetName;
+            var planetNameResult = PlanetName.TryParse(planetNameStr, out planetName);
+            var geoLocationResult = await Tools.AddressToGeoLocation(locationName);
+            var geoLocation = geoLocationResult.Payload;
+
+            //check result 1st before parsing
+            if (!planetNameResult || !geoLocationResult.IsPass) { return APITools.FailMessage("Please check your input, it failed to parse. Check the caps/spelling/parameter order!", incomingRequest); }
+
+            //clean time text
+            var timeStr = $"{hhmmStr} {dateStr}/{monthStr}/{yearStr} {offsetStr}";
+            var parsedTime = new Time(timeStr, geoLocation);
+
+            //based on property call the method
+            var returnVal = "";
+            switch (propertyName1)
+            {
+
+                //LONGITUDE
+                case "Longitude":
+                    {
+                        switch (propertyName2)
+                        {
+                            case "Fixed":
+                            case "Nirayana": returnVal = AstronomicalCalculator.GetPlanetNirayanaLongitude(parsedTime, planetName).ToString(); break;
+                            case "Movable":
+                            case "Sayana": returnVal = AstronomicalCalculator.GetPlanetSayanaLongitude(parsedTime, planetName).ToString(); break;
+                        }
+                        break;
+                    }
+                case "Strength":
+                    {
+                        switch (propertyName2)
+                        {
+                            case "Total":
+                            case "ShadbalaPinda": returnVal = AstronomicalCalculator.GetPlanetShadbalaPinda(planetName, parsedTime).ToString(); break;
+
+                            case "Positional":
+                            case "Sthana": returnVal = AstronomicalCalculator.GetPlanetSthanaBala(planetName, parsedTime).ToString(); break;
+
+                            case "Directional":
+                            case "Dig": returnVal = AstronomicalCalculator.GetPlanetDigBala(planetName, parsedTime).ToString(); break;
+                            
+                            case "Temporal":
+                            case "Kala": returnVal = AstronomicalCalculator.GetPlanetKalaBala(planetName, parsedTime).ToString(); break;
+
+                            case "Motional":
+                            case "Chesta": returnVal = AstronomicalCalculator.GetPlanetChestaBala(planetName, parsedTime).ToString(); break;
+
+                            case "Natural":
+                            case "Naisargika": returnVal = AstronomicalCalculator.GetPlanetNaisargikaBala(planetName, parsedTime).ToString(); break;
+
+                            case "Aspect":
+                            case "Drik": returnVal = AstronomicalCalculator.GetPlanetDrikBala(planetName, parsedTime).ToString(); break;
+                        }
+                        break;
+                    }
+
             }
 
 

ee88f86bdc88e2d8e77c8b71405978576e2ddcaf
VedAstroDeveloper <developer@vedastro.org>
2023-03-29 14:54:19 +0800

    versioning cleanup

--- Library/objects/ThisAssembly.cs
+++ Library/objects/ThisAssembly.cs
@@ -6,10 +6,10 @@
     public static class ThisAssembly
     {
         //HARD CODED DEFAULTS
-        public const string CommitHash = "d4e6327";
+        public const string CommitHash = "0000000";
         public const string CommitNumber = "000";
         public const string BranchName = "stable";
-        public const string Version = "d4e6327-000-stable";
+        public const string Version = "0000000-000-stable";
     }
 
 }

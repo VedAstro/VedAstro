eadf1ca84e327d68c380a9b2facaef8965f3e458
VedAstroDeveloper <developer@vedastro.org>
2023-04-07 13:50:14 +0800

    desktop app proto added

---
+++ Desktop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Desktop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}

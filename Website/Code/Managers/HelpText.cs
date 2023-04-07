fbf5a0fda0fd6ecfa1761deee8c9e5ab20d96a4e
VedAstroDeveloper <developer@vedastro.org>
2023-04-07 18:15:07 +0800

    help text update

--- Website/Code/Managers/HelpText.cs
+++ Website/Code/Managers/HelpText.cs
@@ -8,8 +8,8 @@
     {
         public const string GeoLocationName = @"Enter city or state name";
         public const string DasaTimeRange = @"Select time to show in Dasa report";
-        public const string Gender = @"Gender is used in predicting marriage compatibilty and horoscope";
-        public const string PrivateProfileName = @"Names are private. Use real names for easy identification.";
+        public const string Gender = @"Gender is used in predicting marriage compatibility and horoscope";
+        public const string PrivateProfileName = @"Use unique names for easy sorting later";
         public const string PersonAdvanced = @"This is optional";
         public const string DateInput = @"Format Example: 5th January 2020 is 05/01/2020";
         public const string TimezoneInput = @"Standard timezone used at location (UTC/GMT)";
@@ -21,7 +21,7 @@
         public const string AskAstrologerQuestion = @"Common questions to ask the astrologer";
         public const string AskAstrologerDetails = @"Give extra info regarding your question";
         public const string DasaChart = @"This chart shows accurate Dasa, Bhukti, Antaram and Gochara for person's life";
-        public const string MuhurthaChart = @"This chart shows when good and bad muhurtha events will happen";
+        public const string MuhurthaChart = @"This chart shows when good and bad Muhurtha events will happen";
         public const string SearchReference = @"Astrological facts used for quick reference";
         public const string SearchPredictions = @"These are horoscope predictions that have been programmed into VedAstro";
         public const string SearchEvents = @"These are Muhurtha events that have been programmed into VedAstro";

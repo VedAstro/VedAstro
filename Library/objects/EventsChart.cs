cf12d1090257910f780f0da03882da90b0ec9406
VedAstroDeveloper <developer@vedastro.org>
2023-05-30 11:11:47 +0800

    chart update

--- Library/objects/EventsChart.cs
+++ Library/objects/EventsChart.cs
@@ -82,20 +82,7 @@ namespace VedAstro.Library
         /// Use CHART ID instead if possible
         /// Gets HASH of chart ID, reliable
         /// </summary>
-        public override int GetHashCode()
-        {
-            return Tools.GetStringHashCode(ChartId); ;
-
-            ////get hash of all the fields & combine them
-            //var hash1 = PersonId.GetHashCode();
-            //var hash2 = Tools.GetStringHashCode(StartTimeXml.ToString());
-            //var hash3 = Tools.GetStringHashCode(EndTimeXml.ToString());
-            //var hash4 = Tools.GetStringHashCode(EventTagListXml.ToString());
-
-            ////take out negative before returning
-            //var hashCode = Math.Abs(hash1 + hash2 + hash3 + hash4);
-            //return hashCode;
-        }
+        public override int GetHashCode() => Tools.GetStringHashCode(ChartId);
 
         /// <summary>
         /// Gets a nice identifiable name for this chart to show user 
@@ -111,17 +98,18 @@ namespace VedAstro.Library
 
         /// <summary>
         /// create a unique signature to identify all future calls that is exactly alike
-        /// todo use this for DURABALE ID when caching chart calls
         /// </summary>
         public string GetEventsChartSignature()
         {
-
             //convert events into 1 signature
             var eventTagsHash = 0;
             foreach (var eventTag in this.EventTagList) { eventTagsHash += (int)eventTag; }
 
             //convert input data into a signature
-            var dataSignature = this.PersonId + this.StartTime.GetHashCode() + this.EndTime.GetHashCode() + this.DaysPerPixel + eventTagsHash;
+            //var dataSignature = this.PersonId + this.StartTime.GetHashCode() + this.EndTime.GetHashCode() + this.DaysPerPixel + eventTagsHash;
+
+            //use ticks because can revert back from there
+            var dataSignature = $"{this.PersonId}-{this.StartTime.GetStdDateTimeOffset().Ticks}-{this.EndTime.GetStdDateTimeOffset().Ticks}-{this.DaysPerPixel}-{eventTagsHash}";
 
             return dataSignature;
         }
@@ -130,11 +118,8 @@ namespace VedAstro.Library
         /// <summary>
         /// Creates empty from json SPEC ONLY
         /// </summary>
-        /// <param name="requestJson"></param>
-        /// <returns></returns>
         public static async Task<EventsChart> FromJsonSpecOnly(JObject? requestJson)
         {
-
 
             //get all the data needed out of the incoming request
             //var rootXml = await APITools.ExtractDataFromRequestXml(req);

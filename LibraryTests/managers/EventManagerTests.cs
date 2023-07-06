555f245f5dfa691d53a44c8e42cd66691015d54f
Vignes <vignes@live.com.my>
2023-07-06 14:13:39 +0800

    Library test project proto

---
+++ LibraryTests/managers/EventManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VedAstro.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VedAstro.Library.Tests
+{
+    [TestClass()]
+    public class EventManagerTests
+    {
+
+        /// <summary>
+        /// Debugger to make sure all scenarios for event generation from slices work
+        /// </summary>
+        [TestMethod()]
+        public void EventSlicesToEventsTest()
+        {
+            //case 1 : 1 event in middle of 2 nulls
+
+
+            EventManager.EventSlicesToEvents(null, null, null, null, new[] { 1, 2});
+
+            Assert.Fail();
+        }
+    }
+}

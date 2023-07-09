a564b752ab758d34de07284bcefd10942fddd5a6
Vignes <vignes@live.com.my>
2023-07-09 19:03:03 +0800

    url shortening

--- Website/Code/Managers/PageRoute.cs
+++ Website/Code/Managers/PageRoute.cs
@@ -36,26 +36,26 @@
         
         //CALCULATORS
         public const string CalculatorList = "Calculator/";
-        public const string LifePredictor = "Calculator/LifePredictor";
-        public const string LifePredictorParam = "Calculator/LifePredictor/{PersonIdUrl}";
-        public const string StarsAboveMe = "Calculator/StarsAboveMe";
-        public const string SunRiseSetTime = "Calculator/SunRiseSetTime";
-        public const string BirthTimeFinder = "Calculator/BirthTimeFinder";
-        public const string LocalMeanTime = "Calculator/LocalMeanTime";
-        public const string Horoscope = "Calculator/Horoscope";
-        public const string HoroscopeParam = "Calculator/Horoscope/{PersonIdUrl}";
-        public const string Muhurtha = "Calculator/Muhurtha";
-        public const string FamilyChart = "Calculator/FamilyChart";
-        public const string MatchReport = "Calculator/Match/Report";
-        public const string APIBuilder = "Calculator/APIBuilder";
+        public const string LifePredictor = "LifePredictor";
+        public const string LifePredictorParam = "LifePredictor/{PersonIdUrl}";
+        public const string StarsAboveMe = "StarsAboveMe";
+        public const string SunRiseSetTime = "SunRiseSetTime";
+        public const string BirthTimeFinder = "BirthTimeFinder";
+        public const string LocalMeanTime = "LocalMeanTime";
+        public const string Horoscope = "Horoscope";
+        public const string HoroscopeParam = "Horoscope/{PersonIdUrl}";
+        public const string Muhurtha = "Muhurtha";
+        public const string FamilyChart = "FamilyChart";
+        public const string MatchReport = "Match/Report";
+        public const string APIBuilder = "APIBuilder";
 
 
         //MATCH
-        public const string Match = "Calculator/Match";
-        public const string SavedMatchReports = "Calculator/Match/Saved";
-        public const string MatchProfile = "Calculator/Match/Profile";
-        public const string MatchReportParam = "Calculator/Match/Report/{MaleId}/{FemaleId}";
-        public const string MatchFinder = "Calculator/Match/Finder";
+        public const string Match = "Match";
+        public const string SavedMatchReports = "Match/Saved";
+        public const string MatchProfile = "Match/Profile";
+        public const string MatchReportParam = "Match/Report/{MaleId}/{FemaleId}";
+        public const string MatchFinder = "Match/Finder";
 
 
 

4fe4c4861a7fc148115ea809ceaeed64a3657529
Vignes <vignes@live.com.my>
2023-09-03 19:12:47 +0800

    Catch404 bug revert back

--- API/OpenAPI.cs
+++ API/OpenAPI.cs
@@ -73,15 +73,15 @@ namespace API
         /// <summary>
         /// Backup function to catch invalid calls, say gracefully fails
         /// </summary>
-        [Function(nameof(Catch404))]
-		public static async Task<HttpResponseData> Catch404([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{*Catch404}")]
-			HttpRequestData incomingRequest,
-			string Catch404
-		)
-		{
-			var message = "Invalid or Outdated Call, please rebuild API URL at vedastro.org/APIBuilder";
-	        return APITools.FailMessageJson(message, incomingRequest);
-		}
+  //      [Function(nameof(Catch404))]
+		//public static async Task<HttpResponseData> Catch404([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{*Catch404}")]
+		//	HttpRequestData incomingRequest,
+		//	string Catch404
+		//)
+		//{
+		//	var message = "Invalid or Outdated Call, please rebuild API URL at vedastro.org/APIBuilder";
+	 //       return APITools.FailMessageJson(message, incomingRequest);
+		//}
 
 
 		/// <summary>

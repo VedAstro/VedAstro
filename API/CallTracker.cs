0566fbe73625729d223008539bddcf108a13aee9
Vignes <vignes@live.com.my>
2023-09-04 12:11:50 +0800

    clean up

--- API/CallTracker.cs
+++ API/CallTracker.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Data.Tables;
+using VedAstro.Library;
 
 namespace API;
 
@@ -15,7 +16,7 @@ public static class CallTracker
         //todo cleanup
         var storageUri = $"https://vedastroapistorage.table.core.windows.net/{tableName}";
         string accountName = "vedastroapistorage";
-        string storageAccountKey = "kquBbAE8QKhe/Iyny4F0we3Yx6Y/zJv7yZgrERuFq7nRoEa53o6rb50W88II+PsSEP7RzYYNQizDHPOlt0kwxw==";
+        string storageAccountKey = Secrets.VedAstroApiStorageKey;
 
         //save reference for late use
         tableServiceClient = new TableServiceClient(new Uri(storageUri), new TableSharedKeyCredential(accountName, storageAccountKey));
@@ -51,7 +52,6 @@ public static class CallTracker
     /// <summary>
     /// Marks the call as running
     /// </summary>
-    /// <param name="callerId"></param>
     public static void CallStart(string callerId)
     {
         //set the call as running

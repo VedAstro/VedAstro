0e4618d134f5b752388bb4c7826d59b1c0a52fe8
Vignes <vignes@live.com.my>
2023-09-04 12:12:18 +0800

    wip open api logger

---
+++ API/OpenAPICallLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
+
+namespace API
+{
+
+	/// <summary>
+	/// Represents the data in 1 row of OpenAPILogBook
+	/// </summary>
+	public class OpenAPILogBookEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string IpAddress { get; set; }
+        public bool URL { get; set; }
+		public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+	}
+}

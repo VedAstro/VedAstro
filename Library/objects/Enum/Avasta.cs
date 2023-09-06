5abb3e16372bbd8a74ef3d078387ed712e1651f1
Vignes <vignes@live.com.my>
2023-09-06 17:42:10 +0800

    avasta first step, make enum :D

---
+++ Library/objects/Enum/Avasta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VedAstro.Library
+{
+
+	public enum Avasta
+	{
+		/// <summary>
+		/// Kshudita/hungry 
+		/// </summary>
+		KshuditaStarved,
+
+		/// <summary>
+		/// Trashita/thirsty 
+		/// </summary>
+		TrishitaThirst,
+
+		/// <summary>
+		/// Lajjita/humiliated
+		/// </summary>
+		LajjitaShamed,
+
+		/// <summary>
+		/// Garvita/proud 
+		/// </summary>
+		GarvitaProud,
+
+		/// <summary>
+		/// Mudita / sated/happy 
+		/// </summary>
+		MuditaDelighted,
+
+		/// <summary>
+		/// Kshobhita/guilty/repentant 
+		/// </summary>
+		KshobitaAgitated
+	}
+}

5615f8ee8726d25eff57a385abd2a4d3f1e7b89d
Vignes <vignes@live.com.my>
2023-09-09 05:03:22 +0800

    NEW Lunar & Solar Eclipse API

--- LibraryTests/managers/Calculator/AstronomicalCalculatorTests.cs
+++ LibraryTests/managers/Calculator/AstronomicalCalculatorTests.cs
@@ -8,39 +8,46 @@ using System.Threading.Tasks;
 
 namespace VedAstro.Library.Tests
 {
-    [TestClass()]
-    public class AstronomicalCalculatorTests
-    {
-        public static Time StandardHoroscope = new("14:20 16/10/1918 +05:30", GeoLocation.Bangalore);
+	[TestClass()]
+	public class AstronomicalCalculatorTests
+	{
+		public static Time StandardHoroscope = new("14:20 16/10/1918 +05:30", GeoLocation.Bangalore);
 
-        [TestMethod()]
-        public void GeoLocationTest()
-        {
-	        var x = new GeoLocation("Tokyo", 35.6895, 139.6917);
+		[TestMethod()]
+		public void GeoLocationTest()
+		{
+			var x = new GeoLocation("Tokyo", 35.6895, 139.6917);
 
 
-        }
+		}
 
 
 		[TestMethod()]
-        public void GetAllBhinnashtakavargaChartTest()
-        {
+		public void GetAllBhinnashtakavargaChartTest()
+		{
 
-            var bhinnashtakavargaChart = Calculate.AllBhinnashtakavargaChart(StandardHoroscope);
+			var bhinnashtakavargaChart = Calculate.AllBhinnashtakavargaChart(StandardHoroscope);
 
-            //correct answer for Standard Horoscope from Ashtakavarga System pg.18            
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Aries]);
-            Assert.AreEqual(3,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Taurus]);
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Gemini]);
-            Assert.AreEqual(4,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Cancer]);
-            Assert.AreEqual(4,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Leo]); //todo in book is 5 here is 4 could be rounding
-            Assert.AreEqual(4,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Virgo]);
-            Assert.AreEqual(3,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Libra]);
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Scorpio]);
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Sagittarius]);
-            Assert.AreEqual(0,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Capricornus]);
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Aquarius]);
-            Assert.AreEqual(5,bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Pisces]);
+			//correct answer for Standard Horoscope from Ashtakavarga System pg.18            
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Aries]);
+			Assert.AreEqual(3, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Taurus]);
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Gemini]);
+			Assert.AreEqual(4, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Cancer]);
+			Assert.AreEqual(4, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Leo]); //todo in book is 5 here is 4 could be rounding
+			Assert.AreEqual(4, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Virgo]);
+			Assert.AreEqual(3, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Libra]);
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Scorpio]);
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Sagittarius]);
+			Assert.AreEqual(0, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Capricornus]);
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Aquarius]);
+			Assert.AreEqual(5, bhinnashtakavargaChart[PlanetName.Sun][ZodiacName.Pisces]);
+		}
+
+		[TestMethod()]
+		public void NextLunarEclipseTest()
+		{
+			var x = Calculate.NextLunarEclipse(Time.Now(GeoLocation.Bangkok));
+            Assert.Fail();
         }
     }
 }

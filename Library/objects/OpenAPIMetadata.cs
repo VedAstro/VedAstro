6c68d433f7b4e919268cfef08421509f583b36b4
Vignes <vignes@live.com.my>
2023-09-22 04:11:42 +0800

    Open API metadata static table

---
+++ Library/objects/OpenAPIMetadata.cs
@@ -0,0 +1,31 @@
+﻿namespace VedAstro.Library;
+
+
+/// <summary>
+/// static meta data for a given method in Open API
+/// </summary>
+public class OpenAPIMetadata
+{
+    public OpenAPIMetadata()
+    {
+    }
+
+    public OpenAPIMetadata(string description, string exampleOutput, string signature)
+    {
+        Description = description;
+        ExampleOutput = exampleOutput;
+        Signature = signature;
+    }
+
+    /// <summary>
+    /// includes full method signature in string to ID the method
+    /// </summary>
+    public string Signature { get; set; }
+
+    /// <summary>
+    /// comment from code injected here
+    /// </summary>
+    public string Description { get; set; }
+
+    public string ExampleOutput { get; set; }
+}

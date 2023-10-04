46541b16b549cdb6899a0c1615e1a8dee5de97dd
Vignes <vignes@live.com.my>
2023-10-04 21:11:15 +0800

    clean up

---
+++ Library.API/MLTableTools.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using VedAstro.Library;
+using Microsoft.AspNetCore.Components.Forms;
+
+
+namespace Library.API;
+
+
+/// <summary>
+/// Code to generate ML Table via Open API from a web/desktop client
+/// </summary>
+public class MLTableTools
+{
+
+    private readonly VedAstroAPI _api;
+
+
+    //PUBLIC
+
+    public MLTableTools(VedAstroAPI vedAstroApi) => _api = vedAstroApi;
+
+
+    /// <summary>
+    /// Given an excel file uploaded by user, send to server to process and extract out a Time list
+    /// </summary>
+    public async Task<List<Time>> GetTimeListFromExcel(byte[] inputedFile)
+    {
+
+        //prepare url to call
+        var url = $"{_api.URL.GetMLTimeListFromExcel}/UserId/{_api.UserId}/VisitorId/{_api.VisitorID}";
+
+        //make call
+        var results = await _api.GetList(url, Time.FromJsonList);
+
+        return results;
+    }
+
+    public async Task<byte[]> GenerateMLTableExcel(List<Time> timeList, List<OpenAPIMetadata> columnNameList)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Library.API/MatchTools.cs
@@ -0,0 +1,28 @@
+﻿using VedAstro.Library;
+
+namespace Library.API;
+
+public class MatchTools
+{
+    private readonly VedAstroAPI _api;
+
+
+    public MatchTools(VedAstroAPI vedAstroApi)
+    {
+        _api = vedAstroApi;
+    }
+
+    public async Task<List<PersonKutaScore>> GetList(string personId)
+    {
+        //CHECK CACHE
+        //cache will be cleared when update is needed
+        //if (CachedPersonKutaScore.Any()) { return CachedPersonKutaScore; }
+
+        //prepare url to call
+        var url = $"{_api.URL.FindMatch}/PersonId/{personId}";
+        var personKutaScore = await _api.GetList(url, PersonKutaScore.FromJsonList);
+
+        return personKutaScore;
+
+    }
+}

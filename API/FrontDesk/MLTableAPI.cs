88fd52245ac5498b2a92ecd8e7c30ad8afec8551
Vignes <vignes@live.com.my>
2023-10-05 11:23:53 +0800

    api up & running on local

--- API/FrontDesk/MLTableAPI.cs
+++ API/FrontDesk/MLTableAPI.cs
@@ -18,30 +18,37 @@ namespace API
         /// Generates Time List from an excel file uploaded by user to be parsed and returned as Time list
         /// </summary>
         [Function(nameof(GetMLTimeListFromExcel))]
-        public static async Task<HttpResponseData> GetMLTimeListFromExcel([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = nameof(GetMLTimeListFromExcel))] HttpRequestData incomingRequest)
+        public static async Task<HttpResponseData> GetMLTimeListFromExcel(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post,get", Route = nameof(GetMLTimeListFromExcel))]
+            HttpRequestData incomingRequest)
         {
             //0 : LOG CALL
             //log ip address, call time and URL
             var call = APILogger.Visit(incomingRequest);
 
             //1 : GET DATA OUT TODO NEEDS CHECKING
-            Time x = await Time.FromUrl(timeUrl);
-            var timeAtLocation = x.GetStdDateTimeOffset();
-            var geoLocation = x.GetGeoLocation();
-
-            //2 : CALCULATE
-            var timezoneStr = GeoLocationToTimezone_Vedastro(geoLocation, timeAtLocation);
-            //use google only if no cache in VedAstro
-            if (string.IsNullOrEmpty(timezoneStr))
-            {
-                timezoneStr = await GeoLocationToTimezone_Google(geoLocation, timeAtLocation);
-                //add new data to cache, for future speed up
-                AddToCache(geoLocation, rowKeyData: timeAtLocation.Ticks.ToString(), timezone: timezoneStr);
-            }
+            //get data out of call
+            var rootJson = await APITools.ExtractDataFromRequestJson(incomingRequest);
 
 
-            //3 : SEND TO CALLER
-            return APITools.PassMessage(timezoneStr, incomingRequest);
+            throw new NotImplementedException();
+            //Time x = await Time.FromUrl(timeUrl);
+            //var timeAtLocation = x.GetStdDateTimeOffset();
+            //var geoLocation = x.GetGeoLocation();
+
+            ////2 : CALCULATE
+            //var timezoneStr = GeoLocationToTimezone_Vedastro(geoLocation, timeAtLocation);
+            ////use google only if no cache in VedAstro
+            //if (string.IsNullOrEmpty(timezoneStr))
+            //{
+            //    timezoneStr = await GeoLocationToTimezone_Google(geoLocation, timeAtLocation);
+            //    //add new data to cache, for future speed up
+            //    AddToCache(geoLocation, rowKeyData: timeAtLocation.Ticks.ToString(), timezone: timezoneStr);
+            //}
+
+
+            ////3 : SEND TO CALLER
+            //return APITools.PassMessage(timezoneStr, incomingRequest);
 
         }
 
@@ -49,7 +56,9 @@ namespace API
         /// Generates an instance of ML Table and is sent back wrapped in JSON form
         /// </summary>
         [Function(nameof(GenerateMLTable))]
-        public static async Task<HttpResponseData> GenerateMLTable([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = nameof(GenerateMLTable))] HttpRequestData incomingRequest)
+        public static async Task<HttpResponseData> GenerateMLTable(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = nameof(GenerateMLTable))]
+            HttpRequestData incomingRequest)
         {
             //0 : LOG CALL
             //log ip address, call time and URL
@@ -57,19 +66,24 @@ namespace API
 
             //1 : PREPARE PARAMS DATA
             //prepare url to call
-            var url = $"{_api.URL.GetPersonList}/UserId/{_api.UserId}/VisitorId/{_api.VisitorID}";
-            CachedPersonList = await _api.GetList(url, Person.FromJsonList);
+            //get data out of call
+            var rootJson = await APITools.ExtractDataFromRequestJson(incomingRequest);
+
+            throw new NotImplementedException();
+
+            //var url = $"{_api.URL.GetPersonList}/UserId/{_api.UserId}/VisitorId/{_api.VisitorID}";
+            //CachedPersonList = await _api.GetList(url, Person.FromJsonList);
 
 
-            //2 : GENERATE TABLE (HEAVY COMPUTE 🚀) 
-            var timeSlices = new List<Time>();
-            var openApiMetadatas = new List<OpenAPIMetadata>();
-            var newMLTable =  MLTable.FromData(timeSlices, openApiMetadatas);
+            ////2 : GENERATE TABLE (HEAVY COMPUTE 🚀) 
+            //var timeSlices = new List<Time>();
+            //var openApiMetadatas = new List<OpenAPIMetadata>();
+            //var newMLTable = MLTable.FromData(timeSlices, openApiMetadatas);
 
 
-            //3 : SEND TO CALLER (HTML)
-            var jsonMLTable = newMLTable.ToJson();
-            return APITools.PassMessageJson(jsonMLTable, incomingRequest);
+            ////3 : SEND TO CALLER (HTML)
+            //var jsonMLTable = newMLTable.ToJson();
+            //return APITools.PassMessageJson(jsonMLTable, incomingRequest);
 
 
         }
@@ -78,5 +92,6 @@ namespace API
 
         //----------------------------------PRIVATE FUNCS-----------------------------
 
-       
+
+    }
 }

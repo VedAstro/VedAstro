94cf7da22098fb3a9013aa444cc407dcb6b168b1
Vignes <vignes@live.com.my>
2023-10-07 13:13:20 +0800

    Improved Ayanamsa Data, with Advanced attribute

---
+++ Library/Data/AdvancedOptionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VedAstro.Library
+{
+
+    /// <summary>
+    /// If specified above a field, it is for Pro users and can be used to differentiate in GUI
+    /// It means is is an advanced option, made for use in Ayanamsa
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class AdvancedOptionAttribute : Attribute
+    {
+    }
+}

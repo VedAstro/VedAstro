4fd9ceba335587b5b10cd9bcafa67f5fc484b75f
Vignes <vignes@live.com.my>
2023-10-22 07:06:55 +0800

    buildable

--- API/FrontDesk/OpenAPI.cs
+++ API/FrontDesk/OpenAPI.cs
@@ -9,9 +9,9 @@ namespace API
 {
     public static class OpenAPI
     {
-        //  CALC                          TIME
-        //Karana/Location/Singapore/Time/23:59/31/12/2000/+08:00
-        private const string Route1 = "Calculate/{calculatorName}/{*fullParamString}"; //* that captures the rest of the URL path
+        //.../Calculate/Karana/Location/Singapore/Time/23:59/31/12/2000/+08:00
+        private const string CalculateRoute = $"{nameof(Calculate)}/{{calculatorName}}/{{*fullParamString}}"; //* that captures the rest of the URL path
+        private const string CalculateKPRoute = $"{nameof(CalculateKP)}/{{calculatorName}}/{{*fullParamString}}"; //* that captures the rest of the URL path
 
 
         /// <summary>
@@ -19,7 +19,7 @@ namespace API
         /// /.../Calculator/DistanceBetweenPlanets/PlanetName/Sun/PlanetName/Moon/Location/Singapore/Time/23:59/31/12/2000/+08:00
         /// </summary>
         [Function(nameof(Calculate))]
-        public static async Task<HttpResponseData> Calculate([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Route1)]
+        public static async Task<HttpResponseData> Calculate([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = CalculateRoute)]
             HttpRequestData incomingRequest,
             string calculatorName,
             string fullParamString
@@ -31,7 +31,79 @@ namespace API
                 var callLog = await APILogger.Visit(incomingRequest);
 
                 //1 : GET INPUT DATA
-                var calculator = Tools.MethodNameToMethodInfo(calculatorName); //get calculator name
+                var calculator = Tools.MethodNameToMethodInfo(calculatorName, typeof(Calculate)); //get calculator name
+                var parameterTypes = calculator.GetParameters().Select(p => p.ParameterType).ToList();
+
+                //get inputed parameters
+                var rawOut = await ParseUrlParameters(fullParamString, parameterTypes);
+                var parsedParamList = rawOut.Item1;
+                var remainderParamString = rawOut.Item2; //remainder of chopped string
+
+                //2 : CUSTOM AYANAMSA
+                await ParseAndSetAyanamsa(remainderParamString);
+
+                //3 : EXECUTE COMMAND
+                object rawPlanetData;
+                //when calculator return an async result
+                if (calculator.ReturnType.IsGenericType && calculator.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+                {
+                    dynamic task = calculator.Invoke(null, parsedParamList.ToArray());
+                    await task;
+                    rawPlanetData = task.Result;
+                }
+                //when calculator return a normal result
+                else
+                {
+                    rawPlanetData = calculator?.Invoke(null, parsedParamList.ToArray());
+                }
+
+                //4 : OVERLOAD LIMIT
+                await APITools.AutoControlOpenAPIOverload(callLog);
+
+                //5 : SEND DATA TO CALLER
+                //some calculators return SVG & binary data, so need to send to caller directly
+                switch (calculatorName)
+                {
+                    //handle SVG string
+                    case nameof(VedAstro.Library.Calculate.SkyChart):
+                    case nameof(VedAstro.Library.Calculate.SouthIndianChart):
+                    case nameof(VedAstro.Library.Calculate.NorthIndianChart):
+                        return APITools.SendFileToCaller(System.Text.Encoding.UTF8.GetBytes((string)rawPlanetData), incomingRequest, "image/svg+xml");
+
+                    default:
+                        return APITools.SendAnyToCaller(calculatorName, rawPlanetData, incomingRequest);
+                }
+
+
+            }
+
+            //if any failure, show error in payload
+            catch (Exception e)
+            {
+                APILogger.Error(e, incomingRequest);
+                return APITools.FailMessageJson(e.Message, incomingRequest);
+            }
+
+        }
+
+        /// <summary>
+        /// Main Open API method to handle all calls
+        /// /.../Calculator/DistanceBetweenPlanets/PlanetName/Sun/PlanetName/Moon/Location/Singapore/Time/23:59/31/12/2000/+08:00
+        /// </summary>
+        [Function(nameof(CalculateKP))]
+        public static async Task<HttpResponseData> CalculateKP([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = CalculateKPRoute)]
+            HttpRequestData incomingRequest,
+            string calculatorName,
+            string fullParamString
+            )
+        {
+            try
+            {
+                //0 : LOG CALL : used later for throttle limit
+                var callLog = await APILogger.Visit(incomingRequest);
+
+                //1 : GET INPUT DATA
+                var calculator = Tools.MethodNameToMethodInfo(calculatorName, typeof(VedAstro.Library.CalculateKP)); //get calculator name
                 var parameterTypes = calculator.GetParameters().Select(p => p.ParameterType).ToList();
 
                 //get inputed parameters
@@ -197,67 +269,6 @@ namespace API
             return (parsedParamList, fullParamString);
         }
 
-        private static JToken GetPlanetDataJSON(PlanetName planetName, string propertyName, Time parsedTime, MethodInfo callerToExclude = null)
-        {
-            var individualPropertySelected = !string.IsNullOrEmpty(propertyName);
-
-            //allows to dynamically select property that would other wise come together in list below
-            if (individualPropertySelected)
-            {
-                //find > execute > wrap to JSON matching function property
-                var result = AutoCalculator.ExecuteFunctionsJSON(propertyName, planetName, parsedTime); ;
-
-
-                return result;
-            }
-            //else get all PROPS
-            else
-            {
-                //all planet related data
-                //get all calculators that can accept a planet name and time
-                var xxx = AutoCalculator.FindAndExecuteFunctionsJSON(callerToExclude, planetName, parsedTime); ;
-
-                //send the payload on it's merry way
-                return xxx;
-            }
-
-        }
-
-        private static JToken GetHouseDataJSON(HouseName houseName, string methodName, Time parsedTime, MethodInfo callerToExclude = null)
-        {
-            var individualPropertySelected = !string.IsNullOrEmpty(methodName);
-
-            //allows to dynamically select property that would other wise come together in list below
-            if (individualPropertySelected)
-            {
-                //find > execute > wrap to JSON matching function property
-                var result = AutoCalculator.ExecuteFunctionsJSON(methodName, houseName, parsedTime);
-
-                return result;
-            }
-            //else get all
-            else
-            {
-                //all house related data
-                //get all calculators that can accept a house name and time
-                var houseTimeCalcs = AutoCalculator.FindAndExecuteFunctionsJSON(callerToExclude, houseName, parsedTime);
-
-                //send the payload on it's mary way
-                return houseTimeCalcs;
-            }
-
-
-        }
-
-        private static JObject GetSignDataJson(PlanetName planetName, Time parsedTime)
-        {
-            var planetSign = VedAstro.Library.Calculate.PlanetSignName(planetName, parsedTime);
-            var rootJson = new JObject();
-            rootJson["Name"] = planetSign.GetSignName().ToString();
-            rootJson["DegreesInSign"] = planetSign.GetDegreesInSign().TotalDegrees;
-
-            return rootJson;
-        }
 
     }
 }

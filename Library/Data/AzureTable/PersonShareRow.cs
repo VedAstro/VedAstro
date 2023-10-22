d00122e6705361b7e910de8501052d2392d864f3
Sengiv <vignes@live.com.my>
2023-10-22 09:58:30 +0800

    person share list

---
+++ Library/Data/AzureTable/PersonShareRow.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Data.Tables;
+using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    using Azure.Data.Tables;
+    using Newtonsoft.Json;
+
+namespace VedAstro.Library
+{
+    /// <summary>
+    /// Represents the data in 1 row of person share list table
+    /// represents shared profiles a user has access to made by others
+    /// </summary>
+    public class PersonShareRow : ITableEntity
+    {
+        /// <summary>
+        /// ID of person receiving share and and the person ID of the shared profile
+        /// </summary>
+        public PersonShareRow(string ownerId, string sharedPersonId)
+        {
+            PartitionKey = ownerId;
+            RowKey = sharedPersonId;
+        }
+
+        /// <summary>
+        /// Owner ID of the user receiving the shares
+        /// </summary>
+        public string PartitionKey { get; set; }
+        
+        /// <summary>
+        /// Person ID of the profile shared to this owner
+        /// </summary>
+        public string RowKey { get; set; }
+
+        /// <summary>
+        /// Time of change
+        /// </summary>
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+    }
+
+}

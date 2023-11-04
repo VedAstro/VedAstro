be448114bb5ebdca0f0b813e1c8361039fc30964
Sengiv <vignes@live.com.my>
2023-11-04 16:12:16 +0800

    ML Data Generator basic proto

---
+++ MLDataGenerator/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using MLDataGenerator.Data;
+
+namespace MLDataGenerator
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+
+            return builder.Build();
+        }
+    }
+}

ec07bd737375267289fcb7951c7f170003d2075d
Sengiv <vignes@live.com.my>
2023-11-04 12:45:17 +0800

    clean & bug fixes

--- Library/Data/GeoLocation.cs
+++ Library/Data/GeoLocation.cs
@@ -331,13 +331,23 @@ namespace VedAstro.Library
         /// </summary>
         public static async Task<(bool, GeoLocation)> TryParse(string cellValue)
         {
-            //should return empty 
-            var tryParsed = await GeoLocation.FromName(cellValue);
+            try
+            {
+                //should return empty 
+                var tryParsed = await GeoLocation.FromName(cellValue);
 
-            //if empty than parse failed
-            var isParsed = !(tryParsed.Equals(Empty));
+                //if empty than parse failed
+                var isParsed = !(tryParsed.Equals(Empty));
 
-            return (isParsed, tryParsed);
+                return (isParsed, tryParsed);
+
+            }
+            //if fail for any reason, than empty so caller can know
+            //NOTE: avoid exception or logging here, since failure is expected pattern
+            catch
+            {
+                return (false, Empty);
+            }
         }
 
         /// <summary>

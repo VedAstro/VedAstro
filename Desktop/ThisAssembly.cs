3d203e6880336ce7a42ef4c630db6d00642e330c
Sengiv <vignes@live.com.my>
2023-11-05 08:27:47 +0800

    basic working build proto Desktop

---
+++ Desktop/ThisAssembly.cs
@@ -0,0 +1,18 @@
+
+                    namespace Desktop
+                    {
+                        /// <summary>
+                        /// This file gets set with correct values by publisher before build
+                        /// so version number gets nicely embedded into code just before deployment
+                        /// </summary>
+                        public static class ThisAssembly
+                        {
+                            //HARD CODED DEFAULTS
+                            public const string CommitHash = "8316ff8a";
+                            public const string CommitNumber = "2069";
+                            public const string BranchName = "stable";
+                            public const string Version = "8316ff8a-2069-stable";
+                        }
+                    }                
+                
+            

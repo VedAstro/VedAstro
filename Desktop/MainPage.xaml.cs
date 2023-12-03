1aa95fd4d0910fb361d92db07b73290b7a8481ba
Vignes <vignes@live.com.my>
2023-12-03 23:49:03 +0800

    desktop working build 👌

--- Desktop/MainPage.xaml.cs
+++ Desktop/MainPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace Desktop;
-
-public partial class MainPage : ContentPage
+﻿namespace Desktop
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
 }

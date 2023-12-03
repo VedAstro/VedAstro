a62feb1d58bc9c0f9dd652b989a48a02a03aa6e6
Vignes <vignes@live.com.my>
2023-12-03 17:38:10 +0800

    Working Template & MSIX able also 😉🏓

--- Desktop/Platforms/Tizen/Main.cs
+++ Desktop/Platforms/Tizen/Main.cs
@@ -1,17 +1,16 @@
+using System;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using System;
 
-namespace Desktop
+namespace Desktop;
+
+class Program : MauiApplication
 {
-    internal class Program : MauiApplication
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
-        static void Main(string[] args)
-        {
-            var app = new Program();
-            app.Run(args);
-        }
-    }
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
 }

03a47ced9ae1fef71381fa61daebd66e054c0a90
Vignes <vignes@live.com.my>
2023-12-14 20:37:19 +0800

    dasa options

---
+++ Library/Data/Enum/DasaTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace VedAstro.Library;
+
+public enum DasaType
+{
+    Vimshottari = 1,
+    Ashtottari = 2,
+    Shodshottari = 3,
+    Dvadashottari = 4,
+    Panchottari = 5,
+    Shatabdika = 6,
+    ChaturashitiSama = 7,
+    DwisaptatiSama = 8,
+    Shastihayani = 9,
+    ShattrimshatSama = 10,
+    Chara = 11,
+    Sthira = 12,
+    LagnaKendra = 13,
+    KarakaKendra = 14,
+    Karaka = 15,
+    BrahmaGrahaAshrita = 16,
+    Manduka = 17,
+    Shula = 18,
+    Yogardha = 19,
+    Drg = 20
+}

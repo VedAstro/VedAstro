d3dfcdc580bb0293dad0f65262c00de29ec02857
Vignes <vignes@live.com.my>
2023-12-20 12:04:53 +0800

    working minor updates

--- ViewComponents/Code/API/EventsChartTools.cs
+++ ViewComponents/Code/API/EventsChartTools.cs
@@ -13,13 +13,13 @@ public class EventsChartTools
     /// <summary>
     /// Calls API till get events chart
     /// </summary>
-    public async Task<string> GetEventsChart(Person person, TimeRange timeRange, List<EventTag> inputedEventTags, int maxWidth, ChartOptions options, string ayanamsaName)
+    public async Task<string> GetEventsChart(Person person, TimeRange timeRange, List<EventTag> inputedEventTags, double daysPerPixelRaw, ChartOptions options, string ayanamsaName)
     {
         //no person no entry!
         if (Person.Empty.Equals(person)) { throw new InvalidOperationException("NO CHART FOR EMPTY PERSON!"); }
 
         //generate URL to get chart from API
-        var eventsChartApiCallUrl = GetEventsChartApiUrl(person, timeRange, inputedEventTags, maxWidth, options, ayanamsaName);
+        var eventsChartApiCallUrl = GetEventsChartApiUrl(person, timeRange, inputedEventTags, daysPerPixelRaw, options, ayanamsaName);
 
         //make the call to API, NOTE:call is held here
         var chartString = await _api.PollApiTillDataEventsChart(eventsChartApiCallUrl);
@@ -31,11 +31,11 @@ public class EventsChartTools
     /// Get Events chart api call GET URL that is sent to API,
     /// note: used as a shortcut in website rather going into network tab in F12 
     /// </summary>
-    public string GetEventsChartApiUrl(Person person, TimeRange timeRange, List<EventTag> inputedEventTags, int maxWidth,
+    public string GetEventsChartApiUrl(Person person, TimeRange timeRange, List<EventTag> inputedEventTags, double daysPerPixelRaw,
         ChartOptions summaryOptions, string ayanamsaName)
     {
         //put specs to make chart into a URL format
-        var chartSpecsUrl = EventsChart.FromData(person, timeRange, inputedEventTags, maxWidth, summaryOptions).ToUrl();
+        var chartSpecsUrl = EventsChart.FromData(person, timeRange, inputedEventTags, daysPerPixelRaw, summaryOptions).ToUrl();
 
         //add in server address & API call name
         var finalUrl = _api.URL.GetEventsChart + chartSpecsUrl+$"/Ayanamsa/{ayanamsaName}";

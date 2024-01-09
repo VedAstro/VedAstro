95de3307ca6ad969ccfe84a539294e892229632c
Vignes <vignes@live.com.my>
2024-01-10 00:44:24 +0800

    ToJPEG has landed ✈️🤘

---
+++ Library/Data/Interface/IToJson.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace VedAstro.Library;
+
+/// <summary>
+/// convertible to JSON
+/// </summary>
+public interface IToJson
+{
+
+    /// <summary>
+    /// Converts Type to XML version of it
+    /// </summary>
+    /// <returns></returns>
+    JObject ToJson();
+
+}

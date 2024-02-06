af65d9e3db221b6f214599c066b17098a23547ae
Vignes <vignes@live.com.my>
2024-02-07 07:22:53 +0800

    Hora Math improved, with working test 🧪✅

---
+++ Library/Data/Enum/ChartType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VedAstro.Library
+{
+    public enum ChartType
+    {
+        Rasi,
+        Hora,
+        Drekkana,
+        Chaturthamsa,
+        Panchamsa,
+        Shashthamsa,
+        Saptamsa,
+        Ashtamsa,
+        Navamsa,
+        Dasamsa,
+        Rudramsa,
+        Dwadasamsa,
+        Shodasamsa,
+        Vimsamsa,
+        Chaturvimsamsa,
+        Nakshatramsa,
+        Trimsamsa,
+        Khavedamsa,
+        Akshavedamsa,
+        Shashthyamsa,
+    }
+
+}

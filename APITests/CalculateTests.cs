4a76998f66ce12e373d341dbeec07424f48bd4f3
Vignes <vignes@live.com.my>
2024-02-11 01:49:03 +0800

    1st API Test 🧪 Launched 🚀✈️

---
+++ APITests/CalculateTests.cs
@@ -0,0 +1,27 @@
+﻿using API;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+
+        [TestMethod()]
+        public async Task AllPlanetDataTest()
+        {
+            var json = JObject.Parse(await new HttpClient().GetStringAsync("https://vedicastrogpt.azurewebsites.net/api/Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45"));
+
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+        }
+
+    }
+}

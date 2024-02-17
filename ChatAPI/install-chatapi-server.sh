55341aadfd99e89b5e5601283b24a6cba736845a
Sengiv <vignes@live.com.my>
2024-02-17 11:40:23 +0800

    ACA Chat API working build

--- ChatAPI/install-chatapi-server.sh
+++ ChatAPI/install-chatapi-server.sh
@@ -2,6 +2,25 @@
 
 echo "Installing Chat API Server..."
 
-# Use pip to install the required packages
-# requirements.txt stored in root not src, for easy detection
-pip install -r requirements.txt
+# # Use pip to install the required packages
+# # requirements.txt stored in root not src, for easy detection
+# pip install -r requirements.txt
+
+# #INSTALL .NET 8 SO VEDASTRO CAN WORK NATIVE
+# # Update package lists
+# sudo apt-get update
+
+# # Install pre-requisites
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get install -y dotnet-sdk-8.0
+
+# # Enable Microsoft package feed
+# wget https://packages.microsoft.com/config/debian/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+
+# # Install the .NET Runtime
+# sudo apt-get update
+# sudo apt-get install -y aspnetcore-runtime-8.0
+
+# # Verify the installation
+# dotnet --list-runtimes 

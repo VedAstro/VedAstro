21481674e1dcd49a19e47e4a3bd5e970236d0ad0
Vignes <vignes@live.com.my>
2024-02-27 19:50:29 +0800

    Chat API MK3 finalized! ⛑️

--- ChatAPI/install-chatapi-server.sh
+++ ChatAPI/install-chatapi-server.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-echo "Installing Chat API Server..."
+echo "*****************Installing Chat API Server*****************"
 
-# # Use pip to install the required packages
-# # requirements.txt stored in root not src, for easy detection
-# pip install -r requirements.txt
+# Use pip to install the required packages
+# requirements.txt stored in root not src, for easy detection
+#pip install -r requirements.txt
 
-# #INSTALL .NET 8 SO VEDASTRO CAN WORK NATIVE
-# # Update package lists
-# sudo apt-get update
+#INSTALL .NET SO VEDASTRO CAN WORK NATIVE
+# Install necessary dependencies
+apt-get install -y apt-transport-https gpg ca-certificates wget apt-utils
 
-# # Install pre-requisites
-# sudo apt-get install -y apt-transport-https
-# sudo apt-get install -y dotnet-sdk-8.0
+# Before you install .NET, run the following commands to add the Microsoft package signing key to your list of trusted keys and add the package repository.
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
-# # Enable Microsoft package feed
-# wget https://packages.microsoft.com/config/debian/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
+# Install the runtime set version number here
+apt-get update && \
+   apt-get install -y aspnetcore-runtime-7.0
 
-# # Install the .NET Runtime
-# sudo apt-get update
-# sudo apt-get install -y aspnetcore-runtime-8.0
+# Verify the installation
+dotnet --list-runtimes 
 
-# # Verify the installation
-# dotnet --list-runtimes 
+echo "*****************CHAT SERVER INSTALL COMPLETE*****************"

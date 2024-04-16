a77c33f4e2a10230b6120f03a57b4df4f7eb62a3
Vignes <vignes@live.com.my>
2024-04-16 11:33:45 +0800

    API Tester new life ☘👌

---
+++ APITester/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace APITester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TARGET SERVER
+            string SubDomain = "vedastroapi";
+            string LocalAPIServer = "http://localhost:7071/api/";
+            // string SubDomain = "vedastroapibeta";
+            // string SubDomain = "vedicastrogpt";
+            var appInstance = new TestMethods(SubDomain, LocalAPIServer);
+
+            //PAUSE BEFORE TEST START
+            Console.WriteLine("Press ENTER to start...");
+            Console.ReadLine();
+
+
+        //RUN TESTS
+        RUN_TEST:
+            Console.WriteLine("###### TEST START ######\n");
+            CoalesceException(() => appInstance.GeoLocationToTimezoneTest().Result, null);
+            CoalesceException(() => appInstance.AddressToGeoLocationTest().Result, null);
+
+
+            //HOLD CONTROL
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("###### TEST COMPLETE ######");
+            Console.WriteLine(".....press ENTER to test again");
+            Console.ReadLine();
+
+            //rerun test without restart app
+            goto RUN_TEST;
+        }
+
+
+        //wraps try catch into 1 line (AI made 🤖)
+        public static void CoalesceException<T>(Func<T> expression, T defaultValue)
+        {
+            try
+            {
+                dynamic xx = expression();
+
+                //print call URL to id
+                Console.WriteLine($"## CALL URL --> {xx.URL}");
+                Console.WriteLine($"## OUTPUT :\n{xx.OUTPUT}");//print everything
+                Console.WriteLine("---------------------------------------------------------------------------");
+
+            }
+            catch (Exception ex)
+            {
+
+                //Console.WriteLine(ex.InnerException?.InnerException?.InnerException?.Message);
+                //Console.WriteLine(ex.InnerException?.InnerException?.Message);
+                //Console.WriteLine(ex.InnerException?.Message);
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
+    }
+}
---
+++ APITester/TestMethods.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace APITester
+{
+    public class TestMethods
+    {
+        private string SubDomain { get; set; }
+        private string LocalAPIServer { get; set; }
+
+
+        public TestMethods(string subDomain, string localApiServer)
+        {
+            SubDomain = subDomain;
+            LocalAPIServer = localApiServer;
+        }
+
+        #region GEO_LOCATION
+
+        /// <summary>
+        /// .../AddressToGeoLocation/Gaithersburg, MD, USA
+        /// </summary>
+        public async Task<dynamic> AddressToGeoLocationTest()
+        {
+            //.../Calculate/AddressToGeoLocation/London
+            var url = $"{LocalAPIServer}Calculate/AddressToGeoLocation/Address/London";
+            var json = JObject.Parse(await new HttpClient().GetStringAsync(url));
+
+            //send data back to caller
+            return new { URL = url, OUTPUT = json };
+        }
+
+        /// <summary>
+        /// .../CoordinatesToGeoLocation/Location/SydneyNSW,Australia/Time/14:02/09/11/1977/+00:00
+        /// </summary>
+        public async Task<bool> CoordinatesToGeoLocationTest()
+        {
+            var url =
+                $"{LocalAPIServer}Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45";
+            var json = JObject.Parse(await new HttpClient().GetStringAsync(url));
+
+            Console.WriteLine(json); //print everything
+
+            //check some key values
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+
+            //control comes here once all pass
+            return true; //todo default pass
+
+        }
+
+        /// <summary>
+        /// .../IpAddressToGeoLocation/
+        /// </summary>
+        public async Task<bool> IpAddressToGeoLocationTest()
+        {
+            var url =
+                $"{LocalAPIServer}Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45";
+            var json = JObject.Parse(await new HttpClient().GetStringAsync(url));
+
+            Console.WriteLine(json); //print everything
+
+            //check some key values
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+
+            //control comes here once all pass
+            return true; //todo default pass
+        }
+
+        /// <summary>
+        /// .../Calculate/GeoLocationToTimezone/Location/Tokyo, Japan/Coordinates/35.65,139.83/Time/14:02/09/11/1977/+00:00
+        /// </summary>
+        public async Task<dynamic> GeoLocationToTimezoneTest()
+        {
+            //That’s the last time we changed timezones, but not the first. We’ve actually changed zone 6 times before that in our history. 
+            // 
+            // Prior to 1901, GMT+6.46
+            // After 1901, GMT+6.55 (Set to Singapore Mean Time by Colonial Powers) 
+            // After 1905, GMT+7.00  (Rounded off the time to the Standard Zone Time) 
+            // After 1933, GMT+7.20 (Adjusted for Daylight Savings) 
+            // After 1941, GMT+7.30 (Further Adjusted for Daylight Savings) 
+            // After 1942, GMT+9.00 (Adjusted to Tokyo Standard Time during Japanese Occupation) 
+            // After 1945, GMT+7.30 (Return to Daylight Savings Time) 
+            // After 1982, GMT+8.00 (Malaysia Standard Time) 
+
+            var url = $"{LocalAPIServer}Calculate/GeoLocationToTimezone/Location/Tokyo, Japan/Coordinates/35.65,139.83/Time/14:02/09/11/1977/+00:00";
+            var json = JObject.Parse(await new HttpClient().GetStringAsync(url));
+
+
+            //send data back to caller
+            return new {URL = url, OUTPUT=json};
+        }
+
+        #endregion
+
+        /// <summary>
+        /// .../Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45
+        /// </summary>
+        public async Task AllPlanetDataTest()
+        {
+            var json = JObject.Parse(await new HttpClient().GetStringAsync($"https://{SubDomain}.azurewebsites.net/api/Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45"));
+
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+        }
+
+        /// <summary>
+        /// .../Calculate/AllHouseData/HouseName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45
+        /// </summary>
+        public async Task AllHouseData()
+        {
+            var json = JObject.Parse(await new HttpClient().GetStringAsync($"https://{SubDomain}.azurewebsites.net/api/Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45"));
+
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+        }
+
+        /// <summary>
+        /// .../Calculate/DasaAtRange/Location/Hetauda/Time/09:3[…]/+05:45/Location/Hetauda/Time/09:30/08/07/2093/+05:45
+        /// </summary>
+        public async Task DasaAtRangeTest()
+        {
+            var json = JObject.Parse(await new HttpClient().GetStringAsync($"https://{SubDomain}.azurewebsites.net/api/Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45"));
+
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+        }
+
+        /// <summary>
+        /// .../Calculate/BhinnashtakavargaChart/Location/Hetauda/Time/09:30/08/07/2023/+05:45
+        /// </summary>
+        public async Task BhinnashtakavargaChartTest()
+        {
+            var json = JObject.Parse(await new HttpClient().GetStringAsync($"https://{SubDomain}.azurewebsites.net/api/Calculate/AllPlanetData/PlanetName/All/Location/Hetauda/Time/09:30/08/07/2023/+05:45"));
+
+            Assert.IsTrue(json["Status"]?.Value<string>() == "Pass");
+            Assert.IsTrue(json["Payload"]["AllPlanetData"].HasValues);
+            Assert.IsTrue(json["Payload"]["AllPlanetData"][0]["Sun"].HasValues);
+        }
+
+
+
+    }
+}

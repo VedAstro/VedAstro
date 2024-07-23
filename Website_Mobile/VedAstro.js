d2f69458967c80af097357201a3deeb6f0dd24be
Pratham Upadhyay <63505631+PRATHAMU200@users.noreply.github.com>
2024-07-23 18:40:04 +0530

    Updated Some Home page links and updated mobile nav bar

--- Website_Mobile/VedAstro.js
+++ Website_Mobile/VedAstro.js
@@ -4459,6 +4459,12 @@ class PersonListSelector {
             $(elementdrop).css('max-width', '667px');
             $(elementdrop).html(await this.generateHtmlBodyForFemaleData());
         });
+        //One time DD Script;
+        const elementsofDDScript = document.querySelectorAll("#DDScriptLoader");
+        elementsofDDScript.forEach(async (elementDD) => {
+            $(elementDD).empty();
+            $(elementDD).html(await this.generateHtmlBodyForDDScript());
+        });
 
     }
 
@@ -4506,25 +4512,31 @@ class PersonListSelector {
                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ant-design" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024" data-icon="ant-design:user-add-outlined" data-width="25"><path fill="currentColor" d="M678.3 642.4c24.2-13 51.9-20.4 81.4-20.4h.1c3 0 4.4-3.6 2.2-5.6a371.7 371.7 0 0 0-103.7-65.8c-.4-.2-.8-.3-1.2-.5C719.2 505 759.6 431.7 759.6 349c0-137-110.8-248-247.5-248S264.7 212 264.7 349c0 82.7 40.4 156 102.6 201.1c-.4.2-.8.3-1.2.5c-44.7 18.9-84.8 46-119.3 80.6a373.4 373.4 0 0 0-80.4 119.5A373.6 373.6 0 0 0 137 888.8a8 8 0 0 0 8 8.2h59.9c4.3 0 7.9-3.5 8-7.8c2-77.2 32.9-149.5 87.6-204.3C357 628.2 432.2 597 512.2 597c56.7 0 111.1 15.7 158 45.1a8.1 8.1 0 0 0 8.1.3M512.2 521c-45.8 0-88.9-17.9-121.4-50.4A171.2 171.2 0 0 1 340.5 349c0-45.9 17.9-89.1 50.3-121.6S466.3 177 512.2 177s88.9 17.9 121.4 50.4A171.2 171.2 0 0 1 683.9 349c0 45.9-17.9 89.1-50.3 121.6C601.1 503.1 558 521 512.2 521M880 759h-84v-84c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v84h-84c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h84v84c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-84h84c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8"></path></svg>
              </button>
          </div>
-         <script>
-            var dropdownItems = document.querySelectorAll('.dropdown-item-person-list');
-            var dropdownButton = document.querySelectorAll('.dropdown-toggle-person');
-            dropdownItems.forEach((item) => {
-                item.addEventListener('click', () => {
-                var selectedItemText = item.textContent;
-                var personidvalue = item.getAttribute('data-value');
-                dropdownButton.forEach((button) => {
-                    button.textContent = selectedItemText;
-                    button.value = personidvalue;
-                });
-                //dropdownButton.textContent = selectedItemText;
-                //dropdownButton.value = personidvalue;
-                // Close the dropdown menu
-                document.querySelector('.dropdown-menu').classList.remove('show');
-                });
-            });
-        </script>
+         
      `;
+    }
+
+    async generateHtmlBodyForDDScript(){
+        return `
+        <script>
+                var dropdownItems = document.querySelectorAll('.dropdown-item-person-list');
+                var dropdownButton = document.querySelectorAll('.dropdown-toggle-person');
+                dropdownItems.forEach((item) => {
+                    item.addEventListener('click', () => {
+                    var selectedItemText = item.textContent;
+                    var personidvalue = item.getAttribute('data-value');
+                    dropdownButton.forEach((button) => {
+                        button.textContent = selectedItemText;
+                        button.value = personidvalue;
+                    });
+                    //dropdownButton.textContent = selectedItemText;
+                    //dropdownButton.value = personidvalue;
+                    // Close the dropdown menu
+                    document.querySelector('.dropdown-menu').classList.remove('show');
+                    });
+                });
+            </script>
+        `
     }
 
     async generateHtmlBodyForFemaleData() {

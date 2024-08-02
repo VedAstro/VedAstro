f400c32f218543d4a9d668ed47e82863291ff833
Sengiv <vignes@live.com.my>
2024-08-02 19:49:02 +0800

    updates 👌

---
+++ Library/Secrets-HideMe-sample.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+
+
+//namespace VedAstro.Library
+//{
+
+//    /// <summary>
+//    /// NOTE: declare keys as private to conform proper use
+//    /// keys are designed to be accessed as strings "loosely typed"
+//    /// WHY?
+//    /// so users without keys can still use other features.
+//    /// when loosely typed, lib code will always build
+//    /// </summary>
+//    public static partial class Secrets
+//    {
+//        //MISC
+//        private static string Password = "xxxx";
+//        private static string AutoEmailerConnectString = "xxxx";
+//        private static string BingImageSearch = "xxxxx";
+//        private static string SlackEmailWebhook = "xxxx";
+
+
+//        //STORAGE
+//        private static string CentralStorageAccountName = "xxxx";
+//        private static string CentralStorageKey = "xxxx";
+
+
+//        //GEO LOCATION & TIME & IP
+//        private static string GoogleAPIKey = "xxxx";
+//        private static string AzureMapsAPIKey = "xxxx";
+//        private static string IpDataAPIKey = "xxxxx";
+
+
+//        //LLM
+//        private static string AnyScaleAPIKey = "xxxx";
+//        private static string AzureOpenAIAPIKey = "xxxxx";
+//    }
+//}

2bb479aae4eb880edd1883249d24aa38c05d8d16
Vignes <vignes@live.com.my>
2024-11-05 01:29:02 +0800

    regroup Varga calcs properly 🤡

--- Library/Data/Enum/ChartType.cs
+++ Library/Data/Enum/ChartType.cs
@@ -8,26 +8,22 @@ namespace VedAstro.Library
 {
     public enum ChartType
     {
-        Rasi,
-        Hora,
-        Drekkana,
-        Chaturthamsa,
-        Panchamsa,
-        Shashthamsa,
-        Saptamsa,
-        Ashtamsa,
-        Navamsa,
-        Dasamsa,
-        Rudramsa,
-        Dwadasamsa,
-        Shodasamsa,
-        Vimsamsa,
-        Chaturvimsamsa,
-        Nakshatramsa,
-        Trimsamsa,
-        Khavedamsa,
-        Akshavedamsa,
-        Shashthyamsa,
+        Rasi, //D1
+        Hora, //D2
+        Drekkana, //D3
+        Chaturthamsha, //D4
+        Saptamsha, //D7
+        Navamsha, //D9
+        Dashamsha, //D10
+        Dwadashamsha, //D12
+        Shodashamsha, //D16
+        Vimshamsha, //D20
+        Chaturvimshamsha, //D24
+        Bhamsha, //D27
+        //Trimshamsha, //D30
+        Khavedamsha, //D40
+        Akshavedamsha, //D45
+        Shashtyamsha, //D60
     }
 
 }

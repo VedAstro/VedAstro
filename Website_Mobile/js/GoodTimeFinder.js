f6ea6e4ae3668dee41080bea1151341a2b17c465
Vignes <vignes@live.com.my>
2024-11-08 00:31:48 +0800

    GoodTimeFinder is alive again ⚡⚡⚡BY GOD IT WILL LIVE FOREVER ⚡⚡⚡

--- Website_Mobile/js/GoodTimeFinder.js
+++ Website_Mobile/js/GoodTimeFinder.js
@@ -50,13 +50,15 @@ async function OnClickCalculate() {
     if (!rangeIsValid) { return; } //end here if not valid
 
 
-    //------------------------------OK LETS START-----------------------------
+    //------------------------------ OK LETS START -----------------------------
+
+    //show loading to user
+    CommonTools.ShowLoading();
 
     //update page title with person name to for easy multi tab use (UX ease)
     document.title = `${selectedPerson.DisplayName} | Good Time Finder`;
 
-
-    //------------------------------- CALCULATION STUFF ---------------------
+    //------------------------------- CALL API ---------------------
 
     var selectedPersonId = selectedPerson.PersonId;
 
@@ -70,15 +72,60 @@ async function OnClickCalculate() {
     let daysPerPixel = daysPerPixelInput.getValue();
 
     //construct API call URL in correct format
-    // .../Viknesh1994                     : 0
-    // /Start/00:00/01/01/2011             : 2,3,4,5
-    // /End/00:00/31/12/2024/+08:00        : 7,8,9,10,11
-    // /5.439                              : 12 DaysPerPixel
-    // /PD1,PD2,PD3,PD4,PD5,AshtakvargaGochara,Gochara :13 EventTagList
-    // /GetGeneralScore,GocharaAshtakvargaBindu        :14 SelectedAlgorithm
-    // /Ayanamsa/LahiriChitrapaksha                    :15 Optional Ayanamsa, must be last
-    let apiUrl = `${VedAstro.ApiDomain}/${selectedPersonId}/${timeRangeUrl}/${daysPerPixel}/${selectedEventTags}/${selectedAlgorithms}`;
 
-    console.log(apiUrl);
+    let apiUrl = `${VedAstro.ApiDomain}/EventsChart/${selectedPersonId}/${timeRangeUrl}/${daysPerPixel}/${selectedEventTags}/${selectedAlgorithms}`;
+
+    const fetchApi = async () => {
+        try {
+            const response = await fetch(apiUrl);
+            const callStatus = response.headers.get('Call-Status');
+
+            if (callStatus === 'Running') {
+                await new Promise(resolve => setTimeout(resolve, 10000)); // wait 10 seconds
+                return fetchApi(); // make the call again
+            } else if (callStatus === 'Fail') {
+                throw new Error('API call failed');
+            } else if (callStatus === 'Pass') {
+                const svgString = await response.text();
+                return svgString;
+            } else {
+                throw new Error(`Unknown Call-Status: ${callStatus}`);
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    //get chart from api as SVG string
+    let svgString = await fetchApi();
+
+    // inject SVG string into element with id "EventsChartSvgHolder"
+    document.getElementById("EventsChartSvgHolder").innerHTML = svgString;
+
+    // get id of SVG element
+    let svgElement = document.getElementById("EventsChartSvgHolder").querySelector("svg");
+    let chartId = svgElement.id;
+
+    //create instance
+    //NOTE: unique ID of the chart made by server
+    // chart is available in window.EventsChartList
+    var index = new EventsChart(chartId); //brings to life
+
+    //let caller know all went well
+    console.log(`Amen! Chart Loaded : INDEX:${index}, ID:${chartId}`);
+
+    //make chart holder with buttons visible
+    $('#EventsChartMainElement').show();
+
+    //hide placeholder text
+    $('#EventsChartPlaceHolderMessage').hide();
+
+    //play sound for better UX
+    playBakingDoneSound();
+
+    //hide loading
+    Swal.close();
+
 }
 

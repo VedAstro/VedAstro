684010234a576ef79446ece8c45e42baf938fcab
Vignes <vignes@live.com.my>
2024-11-14 01:58:13 +0800

    contact us page done

---
+++ Website_Mobile/js/ContactUs.js
@@ -0,0 +1,9 @@
+﻿updateHistory();
+
+new PageHeader("PageHeader");
+
+new InfoBox("InfoBox_WhatsApp");
+new InfoBox("InfoBox_Email");
+new InfoBox("InfoBox_Telegram");
+new InfoBox("InfoBox_Skype");
+new InfoBox("InfoBox_Slack");

9d31ec7ce956845e1f0ed62ac7fc119783dd1ec7
Vignes <vignes@live.com.my>
2025-01-04 22:39:12 +0800

    API Builder Proto has landed 🚀

---
+++ Website_Mobile/js/APIBuilder.js
@@ -0,0 +1,21 @@
+﻿updateHistory();
+
+new PageHeader("PageHeader");
+
+var apiMethodViewer = new ApiMethodViewer("ApiMethodViewer");
+
+//var horoscopePersonSelector = new PersonSelectorBox("PersonSelectorBox");
+//var ayanamsaSelector = new AyanamsaSelectorBox("AyanamsaSelectorBox", "RAMAN");
+
+
+//new IconButton("IconButton_Calculate_Horoscope");
+//new IconButton("IconButton_Advanced_Horoscope");
+
+//new InfoBox("InfoBox_AskAI_Horoscope");
+//new InfoBox("InfoBox_EasyImport_Horoscope");
+//new InfoBox("InfoBox_ForgotenTime_Horoscope");
+
+
+//----------------------------------- FUNCS ------------------------------------------
+
+

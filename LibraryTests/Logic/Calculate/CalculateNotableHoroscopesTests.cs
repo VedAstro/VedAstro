a704ff8ac39b91ea3cc48e47a59239cc61e01695
Vignes <vignes@live.com.my>
2025-01-26 01:27:07 +0800

    update

---
+++ LibraryTests/Logic/Calculate/CalculateNotableHoroscopesTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VedAstro.Library.Tests
+{
+    /// <summary>
+    /// All test for book - Notable Horoscopes (1998) : B.V. Raman
+    /// </summary>
+    [TestClass()]
+    public class CalculateNotableHoroscopesTests
+    {
+        [TestMethod()]
+        public void Horoscope63Test()
+        {
+            
+        }
+
+    }
+}
---
+++ LibraryTests/Logic/ToolsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VedAstro.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VedAstro.Library.Tests
+{
+    [TestClass()]
+    public class ToolsTests
+    {
+        [TestMethod()]
+        public void StringToTimezoneTest()
+        {
+            //var xx = Tools.StringToTimezone("00:00");
+            var yx = Tools.StringToTimezone("+00:00");
+            var cc = yx == TimeSpan.Zero;
+        }
+    }
+}

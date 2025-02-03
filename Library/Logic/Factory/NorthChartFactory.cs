ffc15bc92e02407a52d82310295898db309254a7
Sengiv <vignes@live.com.my>
2025-02-03 11:11:58 +0800

    updates

--- Library/Logic/Factory/NorthChartFactory.cs
+++ Library/Logic/Factory/NorthChartFactory.cs
@@ -144,10 +144,10 @@ namespace VedAstro.Library
 
             switch (this.ChartType)
             {
-                case ChartType.Bhava:
+                case ChartType.BhavaChalit:
                     allPlanetsSigns = Calculate.AllPlanetSignsBasedOnHouseLongitudes(time); break;
                 case ChartType.RasiD1:
-                    allPlanetsSigns = Calculate.AllPlanetZodiacSigns(time); break;
+                    allPlanetsSigns = Calculate.AllPlanetRasiSigns(time); break;
                 case ChartType.HoraD2:
                     allPlanetsSigns = Calculate.AllPlanetHoraSign(time); break;
                 case ChartType.DrekkanaD3:
@@ -207,9 +207,10 @@ namespace VedAstro.Library
 
             switch (this.ChartType)
             {
-                case ChartType.Bhava:
-                case ChartType.RasiD1:
+                case ChartType.BhavaChalit:
                     allHousesSigns = Calculate.AllHouseZodiacSigns(this.Time); break;
+                case ChartType.RasiD1:
+                    allHousesSigns = Calculate.AllHouseRasiSigns(this.Time); break;
                 case ChartType.HoraD2:
                     allHousesSigns = Calculate.AllHouseHoraSign(this.Time); break;
                 case ChartType.DrekkanaD3:
